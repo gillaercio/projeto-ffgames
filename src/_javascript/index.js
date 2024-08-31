8b9943b8c40dbc7f08dbd356cfdcd09059c95bf5
Gildman Laércio <laerciokarlagabm@gmail.com>
2024-08-31 16:38:09 -0300

    Refactor code

--- src/_javascript/index.js
+++ src/_javascript/index.js
@@ -4,19 +4,43 @@ const infos = document.querySelectorAll(".info");
 
 carouselButtons.forEach((button, indice) => {
   button.addEventListener("click", () => {
-    const selectedButton = document.querySelector(".selected");
-    selectedButton.classList.remove("selected");
+    disableSelectedButton();
 
-    button.classList.add("selected");
+    markSelectedButton(button);
 
-    const activeImage = document.querySelector(".active");
-    activeImage.classList.remove("active");
+    hideActiveImage();
 
-    images[indice].classList.add("active");
+    showBackgroundimage(indice);
 
-    const activeInfo = document.querySelector(".info.active");
-    activeInfo.classList.remove("active");
+    hideActiveInfo();
 
-    infos[indice].classList.add("active");
+    showInfo(indice);
   });
 });
+
+function disableSelectedButton() {
+  const selectedButton = document.querySelector(".selected");
+  selectedButton.classList.remove("selected");
+}
+
+function markSelectedButton(button) {
+  button.classList.add("selected");
+}
+
+function showBackgroundimage(indice) {
+  images[indice].classList.add("active");
+}
+
+function hideActiveInfo() {
+  const activeInfo = document.querySelector(".info.active");
+  activeInfo.classList.remove("active");
+}
+
+function showInfo(indice) {
+  infos[indice].classList.add("active");
+}
+
+function hideActiveImage() {
+  const activeImage = document.querySelector(".active");
+  activeImage.classList.remove("active");
+}
